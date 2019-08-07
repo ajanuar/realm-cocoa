5617bd795df0087c97e265426619290f18ed3129
Lie Albert Januar Linarco <ajanuar@traveloka.com>
2019-08-07 11:17:59 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -32,9 +32,11 @@ let package = Package(
     products: [
         .library(
             name: "Realm",
+            type: .dynamic,
             targets: ["Realm"]),
         .library(
             name: "RealmSwift",
+            type: .dynamic,
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
